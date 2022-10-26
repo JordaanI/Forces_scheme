7d3a66c526f208c8f3d1112992f1f3e2d9c7eeee
Ivan Jordaan <91161721+JordaanI@users.noreply.github.com>
2022-10-26 10:45:32 -0400

    Updates to V1.1

--- Forces/program.scm
+++ Forces/program.scm
@@ -6,33 +6,59 @@
 ;\\\\\\\\\\\\\        \/     \/    \/     \/     \/ \\\\\\\\\\\\\
 
 (include "functions.scm")
-(define o (open-output-file "data.txt"))
+(define o (open-output-file "data.csv"))
 
+; Runtime in seconds
+(define run_time 30)
+(define t 0.01)
+(define amount (/ run_time t))
 
-; Initialization
+; Initialize particle arrays
 (define 10_mass_array (generate_particles 100 10 500 500))
 (define 5_mass_array (generate_particles 100 5 500 500))
-(define 10ma_force_scale 1)
-(define 5ma_force_scale 3)
-(define t 0.01)
+(define 1_mass_array (generate_particles 100 1 500 500))
 
-; Initialize forces
+; Initialize scalers
+(define 10ma_force_scale 2.63)
+(define 5ma_force_scale 3)
+(define 1ma_force_scale 3.34)
+
+
+; Initialize force arrays
 (define forces_10ma '())
 (define forces_5ma '())
+(define forces_1ma '())
 
 (do ((i 0 (+ i 1)))
-    ((= 3000 i) #!void)
+    ((= amount i) #!void)
   (begin
+    
     ; Update forces between particles
+    
+    ; Update internal forces
     (set! forces_10ma (internal_forces 10ma_force_scale 10_mass_array))
     (set! forces_5ma (internal_forces 5ma_force_scale 5_mass_array))
+    (set! forces_1ma (internal_forces 1ma_force_scale 1_mass_array))
+    
+    ; Update external forces
+    
+    ; Update 10ma
     (set! forces_10ma (external_forces 10ma_force_scale 10_mass_array 5_mass_array forces_10ma))
+    (set! forces_10ma (external_forces 10ma_force_scale 10_mass_array 1_mass_array forces_10ma))
+    
+    ; Update 5ma
     (set! forces_5ma (external_forces 5ma_force_scale 5_mass_array 10_mass_array forces_5ma))
-
+    (set! forces_5ma (external_forces 5ma_force_scale 5_mass_array 1_mass_array forces_5ma))
+    
+    ; Update 10ma
+    (set! forces_1ma (external_forces 1ma_force_scale 1_mass_array 10_mass_array forces_1ma))
+    (set! forces_1ma (external_forces 1ma_force_scale 1_mass_array 5_mass_array forces_1ma))
+    
     ; Update arrays
     (update 10_mass_array forces_10ma t)
     (update 5_mass_array forces_5ma t)
+    (update 1_mass_array forces_1ma t)
 
-    (write_csv o 10_mass_array 5_mass_array)))
+    (write_csv o 10_mass_array 5_mass_array 1_mass_array)))
 
 (close-output-port o)
